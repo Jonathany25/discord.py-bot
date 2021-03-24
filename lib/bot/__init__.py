e75b99e895b27558bb876be46f658c345d0d3097
Jonathany25 <jyunawan@gmail.com>
2021-03-24 13:22:12 +0700

    Added Basic Error Handling Capabilities

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord import Intents
-from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import Bot as BotBase, CommandNotFound
 
 PREFIX = ";"
 OWNER_IDS = [301305436529754113]
@@ -29,6 +29,22 @@ class Bot(BotBase):
 
     async def on_disconnect(self):
         print("Bot Disconnected!")
+
+    async def on_error(self, error, *args, **kwargs):
+        if error == "on_command_error":
+            await args[0].send("Something went wrong!")  # args[0] is an object we can send a message back to
+
+            raise
+
+    async def on_command_error(self, context, exception):
+        if isinstance(exception, CommandNotFound):  # checks if exception is CommandNotFound error
+            pass
+
+        elif hasattr(exception, "original"):
+            raise exception.original  # raises original exception, it is simpler to read
+
+        else:
+            raise exception
 
     async def on_ready(self):
         if not self.ready:
